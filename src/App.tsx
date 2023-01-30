b79fc88d72cd35ce6bce5333ded68001bff92bce
Brian <brianalbertosuarez@gmail.com>
2023-01-30 00:48:48 -0500

    Added functionality to change background menu when scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
+import { useState, useEffect } from "react"
 import Menu from './components/menu/Menu'
 import Home from './components/home/Home'
 import './App.css'
 
 function App() {
+   let [scrollBar, setScrollBar] = useState<boolean>(false)
 
-  return (
-    <div className="App">
-      <Menu />
-      <Home />
-    </div>
-  )
+   useEffect(()=>{
+      const scroll = () => {
+         let scrollY = window.scrollY
+         if(scrollY >= 50){
+            setScrollBar(true)
+         }else if(scrollY < 120){
+            setScrollBar(false)
+         }
+      }
+      window.addEventListener("scroll", scroll)
+    
+   }, [])
+
+   return (
+      <div className="App">
+         <Menu 
+            scrollBar={scrollBar}
+         />
+         <Home />
+      </div>
+   )
 }
 
 export default App
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,16 +1,34 @@
+import { useEffect } from "react"
 import logo from "../../assets/Images/LOGO_CENTRAL.png"
 import { FaBars } from 'react-icons/fa' 
 
-const Menu = () => {
+const Menu = ({scrollBar}) => {
+
    const handleClick = () => {
       const menu = document.getElementById("menu")
       menu?.classList.toggle("collapse")
    }
+
+   useEffect(()=>{
+      const navbar = document.getElementById("navbar")
+      const menu = document.getElementById("menu")
+      const gg = document.getElementById("gg")
+      console.log(navbar)
+      if(scrollBar){
+         navbar?.classList.remove("md:bg-transparent")
+         navbar?.classList.add("md:bg-slate-800")
+         menu?.classList.add("md:bg-slate-800")
+      }else{
+         navbar?.classList.remove("md:bg-slate-800")
+         navbar?.classList.add("md:bg-transparent")
+         menu?.classList.remove("md:bg-slate-800")
+      }
+   },[scrollBar])
    
    return(
-      <div className="w-full bg-slate-800 md:bg-transparent fixed z-10">
-         <nav className="h-11">
-            <div className="flex justify-between md:justify-center md:pb-5">
+      <div>
+         <nav className="h-11 w-full bg-slate-800 md:bg-transparent fixed z-10" id="navbar">
+            <div className="flex justify-between md:justify-center">
                <a href="#"><img className="w-24" src={logo} alt="Logo IBACC" /></a>
                <div className="md:hidden">
                   <button className="flex items-center p-1" onClick={handleClick}>
@@ -19,7 +37,7 @@ const Menu = () => {
                </div>
             </div>
             <div className="bg-slate-800 text-white md:bg-transparent flex justify-center w-full md:visible collapse" id="menu">
-               <ul className="md:flex pb-5">
+               <ul className="md:flex py-5">
                   <li className="md:mx-6 md:text-lg text-center hover:underline hover:cursor-pointer">NOSOTROS</li>
                   <li className="md:mx-6 md:text-lg text-center hover:underline hover:cursor-pointer">UBICACION</li>
                   <li className="md:mx-6 md:text-lg text-center hover:underline hover:cursor-pointer">EN VIVO</li>
